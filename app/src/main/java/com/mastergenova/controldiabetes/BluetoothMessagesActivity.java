9cf2462c866ce3d08bd237c66867d5d4bed30f42
Hugo Henry Sabogal Rodriguez <henrysabogal@localhost.localdomain>
2020-03-02 23:05:17 +0100

    Activity bluetooth

---
+++ app/src/main/java/com/mastergenova/controldiabetes/BluetoothMessagesActivity.java
@@ -0,0 +1,27 @@
+package com.mastergenova.controldiabetes;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class BluetoothMessagesActivity extends AppCompatActivity {
+
+    public static final String TAG = "BluetoothMessagesActivity";
+
+    // Whether the Log Fragment is currently shown
+    private boolean mLogShown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_messages);
+
+    }
+
+}
